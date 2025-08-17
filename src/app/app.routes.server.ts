7d20b99efefc4fdffcda6bc187a2d4ed9b0a425a
Juan Lozada <lozadagomezj@gmail.com>
2025-08-16 23:51:54 -0500

    Arreglo Routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',              // Ruta principal prerenderizada
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'productos',     // Lista general de productos prerenderizada
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'productos',  // Rutas con parámetro se renderizan en cliente
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'contacto',      // Contacto prerenderizado
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '**',             // Otras rutas también prerenderizadas
     renderMode: RenderMode.Prerender
   }
 ];
